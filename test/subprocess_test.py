531a7623adf1822991e3c62960c3b22d97d8754d
Rick Horeman <rickhoreman@gmail.com>
2022-05-24 16:47:53 +0200

    Working c++ to python communication through subprocess and pipe

---
+++ test/subprocess_test.py
@@ -0,0 +1,6 @@
+import subprocess
+
+cpp = subprocess.Popen("", executable="main.exe", stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, bufsize=1)
+while cpp.poll() == None:
+    print(f"cool: {cpp.stdout.readline()}")
+cpp.terminate()
